fba17131ec2f090ecc970a5dd8876db7527dc7fa
Teatoller <steven.otieno1@gmail.com>
2019-02-19 17:08:33 +0300

    feat(profile): implement profile to root reducer
    - import userProfileReducer
    - add user profile reducer to root reducer
    
    [#162949039]

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loginReducer from './LoginReducer/loginReducer';
 import registerReducer from './RegisterReducer/registerReducer';
 import verifyReducer from './VerifyRegristrationReducer/verifyRegistrationReducer';
+import userProfileReducer from './userProfileReducer/userProfileReducer';
 
 const rootReducer = combineReducers({
   login: loginReducer,
   register: registerReducer,
   verifyUser: verifyReducer,
+  userProfile: userProfileReducer,
 });
 
 export default rootReducer;
