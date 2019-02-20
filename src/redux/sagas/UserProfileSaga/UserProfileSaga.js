08b315a25df5824638bb13818b4088600ad675e9
Teatoller <steven.otieno1@gmail.com>
2019-02-20 15:22:15 +0300

    feat(profile): implement user profile saga
    - create user saga
    
    [#162949039]

--- src/redux/sagas/UserProfileSaga/UserProfileSaga.js
+++ src/redux/sagas/UserProfileSaga/UserProfileSaga.js
@@ -2,25 +2,32 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 import * as types from '../../actions/userProfileActions/types';
 
-function* userProfileSaga({ username }) {
+function* userProfileSaga (payload) {
+  const payloadData = payload.payload
+  const { username } = payloadData
+  const { token } = payloadData
+  let config = {
+    headers: {
+      'Authorization': 'token ' + token
+    }
+  }
   try {
-    const response = yield call(axios.get, `https://ah-robotics-staging.herokuapp.com/api/v1/profile/${username.username}/`);
-    console.log(response.data)
+    const response = yield call(axios.get, `https://ah-robotics-staging.herokuapp.com/api/v1/profiles/${username}/`, config);
     yield put({
-      type: types.PROFILE_DATA,
-      payload: { profile: response.data.profile },
+      type: types.PROFILE_DATA_SUCCESS,
+      payload: response.data,
     });
   } catch (err) {
     yield put({
       type: types.PROFILE_ERROR,
-      payload: { errors: err.response.data.errors },
+      payload: { errors: err.response },
     });
   }
 }
 
 function* watchuserProfile() {
   yield takeEvery(
-    types.PROFILE_LOADING, userProfileSaga,
+    types.PROFILE_DATA, userProfileSaga,
 );
 }
 
