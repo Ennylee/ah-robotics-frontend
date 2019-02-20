eb784b3f3303c749f6efe6684174174d8de480e2
Teatoller <steven.otieno1@gmail.com>
2019-02-20 21:59:38 +0300

    feat(profile): implement profile reducer test
    - add test for user profile reducer
    
    [#162949039]

---
+++ src/redux/reducers/userProfileReducer/userProfileReducer.test.js
@@ -0,0 +1,28 @@
+import userProfileReducer, { initialState } from './userProfileReducer';
+import {
+    PROFILE_DATA,
+    PROFILE_ERROR,
+    PROFILE_PATCH_START,
+    PROFILE_PATCH_SUCCESS,
+    PROFILE_PATCH_ERROR,
+    PROFILE_DATA_SUCCESS,
+  } from '../../actions/userProfileActions/types';
+
+describe('resetPassword reducer tests', () => {
+  it('it should have an initial state', () => {
+    expect(userProfileReducer(initialState, {})).toEqual({
+        profileData: {},
+        error: {},
+        profileUpdate: false,
+        isLoading: false,
+        Followers: 0,
+        Following: 0,
+});
+  });
+  it('it should show isloading on profile submit', () => {
+    expect(userProfileReducer(null, {
+      type: PROFILE_DATA_SUCCESS,
+      payload: {},
+    })).toEqual({isLoading: false});
+  });
+});
