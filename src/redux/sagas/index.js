be489e058f9c1c76756e8a40b83b8e1e5ddb3480
Teatoller <steven.otieno1@gmail.com>
2019-02-20 08:51:28 +0300

    feat(profile): implement user profile saga
    - create user profile saga
    
    [#162949039]

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,11 +2,13 @@ import { fork, all } from 'redux-saga/effects';
 import loginWatcher from './LoginSaga/loginSaga';
 import watchRegister from './RegisterSaga/RegisterSaga';
 import watchVerify from './VerifyRegistrationSaga/VerifyRegistrationSaga';
+import watchuserProfile from './UserProfileSaga/UserProfileSaga';
 
 export default function* rootSaga() {
   yield all([
     fork(loginWatcher),
     fork(watchRegister),
     fork(watchVerify),
+    fork(watchuserProfile)
   ]);
 }
