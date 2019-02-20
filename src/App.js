85e93dace45b59ae6a66846a76e6efb04497e6bf
Teatoller <steven.otieno1@gmail.com>
2019-02-20 15:08:58 +0300

    feat(profile): implement profile route
    - add profile route in app.js
    
    [#162949039]

--- src/App.js
+++ src/App.js
@@ -9,27 +9,22 @@ import ResetPasswordView from './views/ResetPasswordView';
 import NavigationComponent from './views/NavigationView';
 import NewPasswordView from './views/NewPasswordView';
 import VerifyRegistrationView from './views/VerifyRegistrationView/index';
+import ProfileView from './views/UserProfileView';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <NavigationComponent />
-          <Switch>
-            <Route path="/" component={HomeView} exact />
-            <Route path="/login" component={LoginView} exact />
-            <Route path="/register" component={RegisterView} />
-            <Route path="/verify/:token" component={VerifyRegistrationView} />
-            <Route path="/resetpassword" component={ResetPasswordView} />
-            <Route path="/reset_password/:token" component={NewPasswordView} />
-            <Route component={Error} />
-
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <NavigationComponent />
+      <Switch>
+        <Route path="/" component={HomeView} exact />
+        <Route path="/login" component={LoginView} exact />
+        <Route path="/register" component={RegisterView} />
+        <Route path="/profiles" component={ProfileView} />
+        <Route path="/verify/:token" component={VerifyRegistrationView} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
--- src/redux/reducers/userProfileReducer/userProfileReducer.js
+++ src/redux/reducers/userProfileReducer/userProfileReducer.js
@@ -2,7 +2,7 @@ import {
   PROFILE_DATA,
   PROFILE_ERROR,
   PROFILE_PATCH,
-  PROFILE_LOADING,
+  PROFILE_DATA_SUCCESS,
   PROFILE_FOLLOWING,
   PROFILE_FOLLOWERS,
 } from '../../actions/userProfileActions/types';
@@ -19,7 +19,22 @@ const initialState = {
 const userProfileReducer = (state = initialState, action) => {
   switch (action.type) {
     case PROFILE_DATA: {
-      return { ...state, profileLoading: true };
+      return { ...state, profileData: action.payload, isLoading: true };
+    }
+    case PROFILE_DATA_SUCCESS: {
+      return { ...action.payload, isLoading: false };
+    }
+    case PROFILE_ERROR: {
+      return { ...state, isLoading: true, errors: action.payload };
+    }
+    case PROFILE_PATCH: {
+      return { ...state, profileUpdate: action.payload };
+    }
+    case PROFILE_FOLLOWING: {
+      return { ...state, Following: action.payload };
+    }
+    case PROFILE_FOLLOWERS: {
+      return { ...state, Followers: action.payload };
     }
     default:
       return state;
